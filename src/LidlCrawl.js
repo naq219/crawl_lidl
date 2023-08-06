a901d9204e5e20c50ef75f2b415306ca7d5820a4
naq_hp <naq0219@gmail.com>
2023-08-06 09:58:53 +0700

    dss

--- src/LidlCrawl.js
+++ src/LidlCrawl.js
@@ -4,15 +4,18 @@ import { MyLog } from './myLog.js';
 import { Sequelize, DataTypes } from 'sequelize';
 import { Database } from './database.js';
 export class lidlCrawl {
+     urls = ['https://www.lidl.de/q/query/supersale'];
+    /**
+     https://www.lidl.de/q/brand/topmove/b1774?sort=discountPercentage-desc&idsOnly=false&productsOnly=false
 
-
+     */
     constructor() {
         Dataset.open();
      }
 
     async crawl1() {
         //this.log2File("startss \n");
-        const url2 = "";
+        
         const url1 = "http://static.lemyde.com/other/lidl_sale.html";
         const url = "https://www-lidl-de.translate.goog/q/query/supersale?sort=deliveryStartDate-desc&idsOnly=false&productsOnly=false&_x_tr_sl=auto&_x_tr_tl=vi&_x_tr_hl=vi&_x_tr_pto=wapp&_x_tr_hist=true"
         const url0="https://www.lidl.de/q/query/supersale?sort=deliveryStartDate-desc&idsOnly=false&productsOnly=true";
@@ -70,20 +73,10 @@ export class lidlCrawl {
 
         
 
-        await crawler.run([url0]);
+        await crawler.run(urls);
 
 
 
-       
-        const abc ={
-            "id": 1,
-            "img": "https://www.lidl.de/assets/gcp43f6c4751a324313b51299d2e2458dd9.jpeg",
-            "title": "Kleinkinder/Kinder Mädchen T-Shirt, 2 Stück, aus reiner Baumwolle",
-            "discount": "0",
-            "price": "\n                    6.99\n                ",
-            "url": "https://www.lidl.de//p/kleinkinder-kinder-maedchen-t-shirt-2-stueck-aus-reiner-baumwolle/p100351636#searchTrackingMasterId=Product.100351636&searchTrackingTitle=Kleinkinder%252FKinder%2BM%25C3%25A4dchen%2BT-Shirt%252C%2B2%2BSt%25C3%25BCck%252C%2Baus%2Breiner%2BBaumwolle&searchTrackingPageSize=24&searchTrackingPage=1&searchTrackingEvent=click&searchTrackingId=Product.100351636&searchTrackingOrigPos=1&searchTrackingPos=1&searchTrackingOrigPageSize=24&searchTrackingChannel=DE&list=search"
-        }
-        //new Database().save(abc);
 
 
 
